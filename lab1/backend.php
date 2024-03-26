59c606125c5acc9fe75e84e7d963b3a28b4ae86f
art-Artur <donaldfak1047@gmail.com>
2024-03-26 04:05:10 +0300

    111

---
+++ lab1/backend.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+    $expression = $data['expression'];
+
+    if (!preg_match('/^[\d\+\-\*\/\(\)\.]*$/', $expression)) {
+        echo json_encode(['error' => 'Некорректные данные']);
+        exit();
+    }
+
+    $result = calculateExpression($expression);
+
+    echo json_encode(['result' => $result]);
+}
+
+function calculateExpression($expression) {
+    // Очищаем ввод от пробелов
+    $expression = str_replace(' ', '', $expression);
+
+    // Проверяем наличие недопустимых символов
+    if (!preg_match('/^[\d\+\-\*\/\(\)\.]*$/', $expression)) {
+        return 'Ошибка';
+    }
+
+    $expression = str_replace('cot', '1/tan', $expression);
+    $expression = str_replace('pi', pi(), $expression);
+
+    $result = eval('return ' . $expression . ';');
+
+    return $result;
+}
+?>
---
+++ lab1/script.js
@@ -0,0 +1,24 @@
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    var expression = document.getElementById('display').value;
+    fetch('backend.php', {
+        method: 'POST',
+        body: JSON.stringify({ expression: expression }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('display').value = data.result;
+    })
+    .catch(error => console.error('Error:', error));
+}
