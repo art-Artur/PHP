770f1a6e49937ac8d8959d89c7125c8c3516e324
art-Artur <145449779+art-Artur@users.noreply.github.com>
2024-06-24 01:56:58 +0300

    Delete projectExpress/projectExpress directory

--- projectExpress/projectExpress/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('project', 'root', '', {
-  host: 'localhost',  
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
--- projectExpress/projectExpress/routes/authorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authorController = require('../controllers/authorController');
-
-router.get('/', authorController.getAllAuthors);
-router.get('/:id', authorController.getArticlesByAuthor);
-
-module.exports = router;
